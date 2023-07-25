9d519e1859c6c0daed7387fa9981d52a5743783b
Gabriel Schultz <gabrielschultz2807@gmail.com>
2023-07-25 12:21:26 -0300

    Adicionado exercício 04 da lista 01

---
+++ curso_programacao/src/lista_de_exercicios/lista_01/exercicio_04.java
@@ -0,0 +1,36 @@
+package lista_de_exercicios.lista_01;
+
+import java.util.Scanner;
+
+
+public class exercicio_04 {
+    
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num;
+		double horas, valor_hora, salario;
+		
+		System.out.println("Calculo de salário\n");
+		
+		System.out.print("Insira o número do funcionário: ");
+		num = sc.nextInt();
+		
+		System.out.print("Insira o número de horas trabalhadas: ");
+		horas = sc.nextDouble();
+		
+		System.out.print("Insira o valor por hora trabalhada: ");
+		valor_hora = sc.nextDouble();
+		
+		salario = horas * valor_hora;
+		
+		System.out.println("\f");
+		
+		System.out.println("Número do funcionário: " + num);
+		System.out.printf("Salário R$ %.2f", salario);
+		
+		
+		sc.close();
+	} 	
+
+}
