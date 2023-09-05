a968ad913f1258d69217782d0548889cf2d97d0b
Gabe <gabrielschultz2807@gmail.com>
2023-09-05 16:23:29 -0300

    Finalizado exercicio 08 da lista 03

--- java_projects/exercicios/lista_de_exercicios/lista_03/exercicio_08/src/application/Program.java
+++ java_projects/exercicios/lista_de_exercicios/lista_03/exercicio_08/src/application/Program.java
@@ -1,10 +1,24 @@
 package application;
 
+import java.util.Scanner;
+
+import arraymath.Pairs;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("How many elements will the array have? ");
+		int length = sc.nextInt();
+		
+		Pairs array = new Pairs(length);
+		array.readValues(sc);
+		
+		array.averageCalc();
+		array.print();
+		
+		sc.close();
 	}
 
 }
--- java_projects/exercicios/lista_de_exercicios/lista_03/exercicio_08/src/arraymath/Pairs.java
+++ java_projects/exercicios/lista_de_exercicios/lista_03/exercicio_08/src/arraymath/Pairs.java
@@ -1,5 +1,44 @@
 package arraymath;
 
-public class Pairs {
+import java.util.Scanner;
 
+public class Pairs {
+	private double[] array;
+	private double averagePairs;
+
+	public Pairs(int length) {
+		this.array = new double[length];
+	}
+
+	public void readValues(Scanner sc) {
+		for(int i = 0; i < array.length; i++) {
+			System.out.print("Type a number: ");
+			array[i] = sc.nextDouble();
+		}
+	}
+
+	public void averageCalc() {
+		double sum = 0;
+		int pairs = 0;
+
+		for(int i = 0; i < array.length; i++) {
+			if (array[i] % 2 == 0) {
+				sum += array[i];
+				pairs ++;
+			}
+		}
+		if (pairs == 0){
+			averagePairs = 0;
+		}else {
+			averagePairs = sum / pairs;
+		}
+	}
+
+	public void print() {
+		if(averagePairs == 0) {
+			System.out.print("No even number.");
+		}else {
+			System.out.print("Peer average: " + String.format("%.1f", averagePairs));
+		}
+	}
 }
