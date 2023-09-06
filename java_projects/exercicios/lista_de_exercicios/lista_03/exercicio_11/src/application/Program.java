4389afea723f9f7cb9c40af1f898170b2249ec26
Gabe <gabrielschultz2807@gmail.com>
2023-09-06 10:20:08 -0300

    Finalizado exercicio 11 da lista 03

--- java_projects/exercicios/lista_de_exercicios/lista_03/exercicio_11/src/application/Program.java
+++ java_projects/exercicios/lista_de_exercicios/lista_03/exercicio_11/src/application/Program.java
@@ -1,10 +1,24 @@
 package application;
 
+import java.util.Scanner;
+
+import dataPerson.Person;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		System.out.print("How many people will be typed? ");
+		int length = sc.nextInt();
+
+		Person people = new Person(length);
+
+		people.readData(sc);
+		people.calcAverage();
+		people.print();
+		
+		sc.close();
 	}
 
 }
--- java_projects/exercicios/lista_de_exercicios/lista_03/exercicio_11/src/dataPerson/Person.java
+++ java_projects/exercicios/lista_de_exercicios/lista_03/exercicio_11/src/dataPerson/Person.java
@@ -1,5 +1,54 @@
 package dataPerson;
 
-public class Person {
+import java.util.Scanner;
 
+public class Person {
+	private double[] height;
+	private char[] gender;
+	private double shortestHeight = Double.MAX_VALUE;
+	private double greaterHeight = Double.MIN_VALUE;
+	private double averageHeigthWoman = 0;
+	private int numberMan = 0;
+
+	public Person(int length) {
+		this.height = new double[length];
+		this.gender = new char[length];
+	}
+
+	public void readData(Scanner sc){
+		for(int i = 0; i < height.length; i++) {
+			System.out.printf("%d person height: ", i+1);
+			height[i] = sc.nextDouble();
+
+			System.out.printf("%d person gender: ", i+1);
+			gender[i] = sc.next().charAt(0);
+		}
+	}
+
+	public void calcAverage() {
+		double sumWoman = 0;
+		int numberWoman = 0;
+
+		for(int i = 0; i < height.length; i++) {	
+			if(shortestHeight > height[i]) {
+				shortestHeight = height[i];
+			}else if(greaterHeight < height[i]) {
+				greaterHeight = height[i];
+			}
+			if(gender[i] == 'F' || gender[i] == 'f') {
+				sumWoman += height[i];
+				numberWoman++;
+			}else if(gender[i] == 'M' || gender[i] == 'm') {
+				numberMan++;
+			}
+		}
+		averageHeigthWoman = sumWoman / numberWoman;
+	}
+
+	public void print() {
+		System.out.println("Shortest Height = " + shortestHeight);
+		System.out.println("Grester Height = " + greaterHeight);
+		System.out.printf("Average height of women = %.2f\n", averageHeigthWoman);
+		System.out.println("Number of men = " + numberMan);
+	}
 }
