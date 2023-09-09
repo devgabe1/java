1f1923e71b5253fc208d41c6db72edca33f788e2
Gabriel Schultz <gabrielschultz2807@gmail.com>
2023-09-09 12:26:07 -0300

    Finalizado exercício 01 de matriz

--- java_projects/exercicios/fixacao/matriz/01/src/application/Program.java
+++ java_projects/exercicios/fixacao/matriz/01/src/application/Program.java
@@ -1,10 +1,40 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		System.out.print("Type the length of the matrix: ");
+		int length = sc.nextInt();
+		
+		int[][] matrix;
+		matrix = new int[length][length];
+		
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix.length; j++) {
+				matrix[i][j] = sc.nextInt();
+			}
+		}
+		System.out.println("Main diagonal:");
+		for(int i = 0; i < matrix.length; i++) {
+			System.out.print(matrix[i][i] + " ");
+		}
+		
+		int count = 0;
+		
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix.length; j++) {
+				if (matrix[i][j] < 0) {
+					count ++;
+				}
+			}
+		}
+		System.out.println();
+		System.out.print("Negative numbers = " + count);
+		sc.close();
 	}
 
 }
