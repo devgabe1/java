67a9e3ed40d2cc2a1e1a1048f27fbc18f9a2ad98
Gabriel Schultz <gabrielschultz2807@gmail.com>
2023-09-10 01:52:07 -0300

    Finalizado exercício 02 de fixação de matriz

--- java_projects/exercicios/fixacao/matriz/02/src/application/Program.java
+++ java_projects/exercicios/fixacao/matriz/02/src/application/Program.java
@@ -1,10 +1,45 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+
+		int[][] matrix = new int[m][n];
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[i].length; j++) {
+				matrix[i][j] = sc.nextInt();
+			}
+		}
+		int number = sc.nextInt();
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[i].length; j++) {
+
+				if(number == matrix[i][j]) {
+					System.out.println("position " + i + "," + j);
+					
+					if(j > 0) {
+						System.out.println("Left: " + matrix[i][j-1]);
+					}
+					if(i > 0) {
+						System.out.println("Up: " + matrix[i-1][j]);
+					}
+					if(j < matrix[i].length - 1) {
+						System.out.println("Right: " + matrix[i][j+1]);
+					}
+					if(i < matrix.length - 1) {
+						System.out.println("Down: " + matrix[i+1][j]);
+					}
+				}
+			}
+		}
+
+		sc.close();
 	}
 
 }
