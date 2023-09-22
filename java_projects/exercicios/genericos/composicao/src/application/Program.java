0344a3872429194ba646fa6ee2d71c218ee62a83
Gabe <gabrielschultz2807@gmail.com>
2023-09-22 17:38:52 -0300

    Criada pasta e projeto server

--- java_projects/exercicios/genericos/composicao/src/application/Program.java
+++ java_projects/exercicios/genericos/composicao/src/application/Program.java
@@ -1,10 +1,22 @@
 package application;
 
+import java.util.Scanner;
+
+import entities.Department;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter department's name: " );
+		Department dep_name = new Department(sc.nextLine());
+		
+		System.out.println("Enter worker data: ");
+		
+		
+		
+		sc.close();
 	}
 
 }
--- java_projects/exercicios/genericos/composicao/src/entities/Worker.java
+++ java_projects/exercicios/genericos/composicao/src/entities/Worker.java
@@ -1,6 +1,7 @@
 package entities;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import entities.enums.WorkerLevel;
@@ -9,12 +10,12 @@ public class Worker {
 	private String name;
 	private WorkerLevel level;
 	private double baseSalary;
-	
+
 	private Department department;
 	private List<HourContract> contracts = new ArrayList<>();
-	
+
 	public Worker() {
-		
+
 	}
 
 	public Worker(String name, WorkerLevel level, double baseSalary, Department department) {
@@ -58,19 +59,27 @@ public class Worker {
 
 	public List<HourContract> getContracts() {
 		return contracts;
-	}
+	}	
 
-	public void setContracts(List<HourContract> contracts) {
-		this.contracts = contracts;
-	}
-	
-	
 	public void addContract(HourContract contract) {
 		contracts.add(contract);
 	}
 	public void removeContracrt(HourContract contract) {
 		contracts.remove(contract);
 	}
-	
-	
+
+	public double income(int year, int month) {
+		double sum = baseSalary;
+		Calendar cal = Calendar.getInstance();
+		for (HourContract c : contracts) {
+			cal.setTime(c.getDate());
+			int c_year = cal.get(Calendar.YEAR);
+			int c_month = 1 + cal.get(Calendar.MONTH);
+			if(year == c_year && month == c_month) {
+				sum += c.totalValue();
+			}
+		}
+		return sum;
+
+	}
 }
